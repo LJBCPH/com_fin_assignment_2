bdab394d5c1a4df5511323928ee46006ef2ad49f
Lucas Johan Boesen <lucasjohanboesen123@gmail.com>
2021-11-04 15:23:13 +0100

    func for mixed model "works"

--- helper_functions.R
+++ helper_functions.R
@@ -3,7 +3,7 @@ calculate_theta_coefs <- function(w, X, Y, C, D) {
          (w * t(X) %*% C + (1 - w) * t(Y) %*% D))
 }
 
-calculate_delta <- function(S0, sigma, Tt, w, K = 1, pol_degree = 7){
+calculate_delta <- function(S0, sigma, Tt, w, K = 1, pol_degree = 7, St = NULL){
   
   rand_norm_T <- rnorm(length(S0))
   S_T <- S0 + sigma * sqrt(Tt) * rand_norm_T
@@ -31,11 +31,28 @@ calculate_delta <- function(S0, sigma, Tt, w, K = 1, pol_degree = 7){
     as_tibble() %>% 
     transmute(D_val = ifelse(value >= K, 1, 0)) %>% 
     pull()
-  # cat("dim X: ", dim(X), 
-  #     "\n dim Y: ", dim(Y),
-  #     "\n dim C: ", length(C))
-  return(list(delta = Y %*% calculate_theta_coefs(w, X, Y, C, D), 
-              D = D))
+
+  coefs <- calculate_theta_coefs(w, X, Y, C, D)
+  if(missing(St)){
+    return(list(delta = Y %*% coefs, 
+                D = D))
+  }else{
+    zeroes <- c(rep(0, length(St)))
+    
+    ones <- c(rep(1, length(St)))
+    
+    Y <- zeroes %>% 
+      cbind(ones) %>% 
+      cbind(poly(St, pol_degree-1, raw=TRUE)) %>% 
+      as.matrix()
+    
+    for(i in 1:(pol_degree+1)){
+      Y[,i] <- Y[,i] * (i-1)
+    }
+    
+    return(list(delta = Y %*% coefs, 
+                D = D))
+  }
 }
 
 est_derr_pricing_function <- function(coefs, S0, pol_degree = 7){
@@ -43,8 +60,7 @@ est_derr_pricing_function <- function(coefs, S0, pol_degree = 7){
   return((S0_6 %*% (c(2:pol_degree) * coefs[2:pol_degree])) + coefs[1])
 }
 
-
-calculate_delta_price <- function(S0, sigma, Tt, K = NULL, w = NULL, pol_degree = 7){
+calculate_delta_price <- function(S0, sigma, Tt, K = NULL, w = NULL, pol_degree = 7, St = NULL){
   rand_norm_T <- rnorm(length(S0))
   S_T <- S0 + sigma * sqrt(Tt) * rand_norm_T
   
@@ -57,23 +73,28 @@ calculate_delta_price <- function(S0, sigma, Tt, K = NULL, w = NULL, pol_degree 
   est_pricing_func <- lm(call_prices ~ poly(S0, pol_degree, raw=TRUE), data = call_simulations)
   
   coefs <- est_pricing_func$coefficients %>% as_tibble() %>% mutate(value = ifelse(is.na(value), 0, value)) %>% as.matrix()
-
-  derr_pricing_func_val <- est_derr_pricing_function(coefs = coefs[-1],
-                                                     S0 = S0, 
-                                                     pol_degree = (pol_degree - 1))
+  if(missing(St)){
+    derr_pricing_func_val <- est_derr_pricing_function(coefs = coefs[-1],
+                                                       S0 = S0, 
+                                                       pol_degree = (pol_degree))
+  } else {
+    derr_pricing_func_val <- est_derr_pricing_function(coefs = coefs[-1],
+                                                       S0 = St, 
+                                                       pol_degree = (pol_degree))
+  }
   return(list(delta = derr_pricing_func_val))
 }
 
-true_delta <- function(S0, K, sigma, Tt, w, pol_degree){
-  return(list(delta = pnorm((S0-K)/(sigma * sqrt(Tt)))))
+true_delta <- function(S0, K, sigma, Tt, w, pol_degree, St){
+  return(list(delta = pnorm((St-K)/(sigma * sqrt(Tt)))))
 }
 
-calculate_hedge_error <- function(dt, Tt, num_rep, K, sigma, St, delta_func, S0, w = NULL, pol_degree = 7){
-  S0 <- S0
+calculate_hedge_error <- function(dt, Tt, num_rep, K, sigma, St, S0, delta_func, w = NULL, pol_degree = 7){
+  #S0 <- S0
   for(t in seq(dt, Tt, dt)){
     St <- St + sigma * sqrt(dt) * rnorm(num_rep)
     Vt <- at * St + bt
-    at <- delta_func(S0 = S0, Tt = (1-t), K = K, sigma = sigma, w = w, pol_degree = 7)$delta
+    at <- delta_func(S0 = S0, Tt = 1-t, K = K, sigma = sigma, w = w, pol_degree = 7, St = St)$delta
     bt <- Vt - at * St
   }
   
